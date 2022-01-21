920cd3f42fe220d122f7170988ac20da7329470f
Geoff Donoghue <geoff.donoghue@mail.utoronto.ca>
2022-01-21 17:18:26 -0500

    Minor commit

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -9,10 +9,34 @@ const BasicView = styled.div<{connected?: boolean}>`
   transform: translateY(650%);
 `
 
+const ExampleCard = styled.div<{ color?: string }>`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  background-color: ${({ theme }) => theme.colors.background.two};
+  border: 2px ${({ theme }) => theme.border.style} ${(props) => props.color}00;
+  border-radius: ${({ theme }) => theme.border.radius};
+  transition: 0.25s box-shadow ease-out, 0.25s border ease-out;
+  width: 290px;
+  min-height: 492px;
+  position: relative;
+  height: 100%;
+  padding: 16px;
+  @media (max-width: ${({ theme }) => theme.sizes.md}px) {
+    width: 343px;
+  }
+  &:hover {
+    box-shadow: ${(props) => props.color}66 8px 16px 80px;
+    border: 2px ${({ theme }) => theme.border.style} ${(props) => props.color};
+  }
+`;
+
 export const HomeView = () => {
   return (
     <BasicView>
-      <div>Home</div>
+      <ExampleCard>
+        Home
+      </ExampleCard>
     </BasicView>
   )
 }
