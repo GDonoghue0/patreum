03bcb9b6964387f8816b0a03d886f92657683325
Geoff Donoghue <geoff.donoghue@mail.utoronto.ca>
2022-02-05 13:30:42 -0500

    Minor commit

--- src/contracts/Fallback.sol
+++ src/contracts/Fallback.sol
@@ -43,3 +43,8 @@ contract Fallback {
     owner = msg.sender;
   }
 }
+
+// Solution:
+// await contract.contribute({value: 100}) // Add a mapping from msg.sender to contributions
+// await contract.sendTransaction({value: 100}) // Trigger receive fallback
+// await contract.withdraw() // Withdraw all funds
